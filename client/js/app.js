3dffd19fd1d53e3b28f66ef352bc76631570492c
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2016-12-14 23:08:41 +0000

    ajouté angularjs

---
+++ client/js/app.js
@@ -0,0 +1,4 @@
+var app = angular.module("myApp",[]);
+app.controller("myCtrl",function($scope){
+		$scope.agents=[{"matricule":"ID83948","nom":"Mohamed El hosayny"},{"matricule":"AO3489","nom":"Jairo Duart"},{"matricule":"I45324","nom":"Dermoumi Said"},{"matricule":"B432442","nom":"Abdeneby Brahim"}];
+});
