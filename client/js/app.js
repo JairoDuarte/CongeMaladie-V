c15080d8de23b0c394b2b221403b47d06b3d28c0
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2016-12-15 14:15:24 +0000

    .

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("myApp",[]);
 
-app.controller("agentList",function($scope){
-		$scope.agents=[{"matricule":"ID83948","nom":"Mohamed El hosayny"},{"matricule":"AO3489","nom":"Jairo Duart"},{"matricule":"I45324","nom":"Dermoumi Said"},{"matricule":"B432442","nom":"Abdeneby Brahim"}];
+app.controller("agentList",function($scope,$http){
+	$scope.agents=[{"matricule":"ID83948","nom":"Mohamed El hosayny"},{"matricule":"AO3489","nom":"Jairo Duart"},{"matricule":"I45324","nom":"Dermoumi Said"},{"matricule":"B432442","nom":"Abdeneby Brahim"}];
 });
 
 app.controller("agentOperationSup",function($scope){
