9406bd5f42d8a14284e47dc7e8798882add6dbea
Jairo Duarte <alfredojairo17@hotmail.com>
2016-12-15 12:07:16 +0000

    .

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var load = require('express-load');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -16,7 +13,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 load('models')
   .then('controllers')
